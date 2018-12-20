5e5395b6a732112d75c0ce76303a1c493babd472
Nicholas Knight <nickmc01@gmail.com>
2018-12-20 10:12:35 -0800

    Admin Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 NetworkMonitor::Application.routes.draw do
   devise_for :users
+
+  
+
   root to: "dashboard#index"
 
   resources :devices
   resources :dashboard
-  
+  resources :users, only: [:show, :edit, :update]
+
   get '/run/:id' => 'ajax#run', as: 'run'
 end
